dd8b3d73916cab10bee66dfddc4cf849664ae35a
G-A223 <gunko.anastassiya@yandex.ru>
2024-07-23 17:58:43 +0900

    initial commit

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
-import tkinter as tk
-
-window = tk.Tk()
-greeting = tk.Label(text="Hello loser!!")
-greeting.pack()
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
 
 
-window.mainloop()
+class Text_processor(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi('choose_option.ui', self)
+
+        self.create_btn.clicked.connect(self.createFile)
+        self.open_btn.clicked.connect(self.openFile)
+
+    def openFile(self):
+        print()
+
+    def createFile(self):
+        print()
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    main = Text_processor()
+    main.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
