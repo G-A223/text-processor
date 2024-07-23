fe7be8eacec1a54615af85f0cf592e831b879f15
G-A223 <gunko.anastassiya@yandex.ru>
2024-07-23 17:58:43 +0900

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+import aspose.words as aw
 from PyQt5 import uic
-
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QTextEdit
 
 class Text_processor(QMainWindow):
     def __init__(self):
@@ -16,7 +16,34 @@ class Text_processor(QMainWindow):
         self.open_btn.clicked.connect(self.openFile)
 
     def openFile(self):
-        print()
+        fileName = QFileDialog.getOpenFileName(self, 'OpenFile')[0]
+        print(fileName)
+        doc = aw.Document(fileName)
+        doc.save("Output.html")
+
+        uic.loadUi('edit.ui', self)
+
+        self.page = QTextEdit()
+        self.page.setStyleSheet("""background-color: rgb(255, 255, 255);
+                                  border-radius: 3px;""")
+
+        with (open('Output.html', 'r', encoding="utf-8") as f):
+            html = f.read()
+
+            indx1 = html.find(
+                "<div style=\"-aw-headerfooter-type:header-primary; clear:both\"><p style=\"margin-top:0pt;")
+            indx2 = (html.find("https://products.aspose.com/words/temporary-license/</span></a></p>")
+                     + len("https://products.aspose.com/words/temporary-license/</span></a></p>"))
+            html = html.replace(html[indx1:indx2], "")
+
+            indx3 = html.find("<div style=\"-aw-headerfooter-type:footer-primary; clear:both\"><p style=\"margin-top:")
+            indx4 = (html.find("2003-2024 Aspose Pty Ltd.</span></p></div>")
+                     + len("2003-2024 Aspose Pty Ltd.</span></p></div>"))
+            html = html.replace(html[indx3:indx4], "")
+
+            self.page.setHtml(html)
+
+        self.txt_field.addWidget(self.page)
 
     def createFile(self):
         print()
